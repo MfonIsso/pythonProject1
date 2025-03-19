58dbbd4b71687d1513771d041c289cf238fbebaa
Mfoniso <122088237+MfonIsso@users.noreply.github.com>
2025-03-19 16:50:40 +0100

    Add files via upload
    this is version 0.0.2 on how to fix command injection/execution attacks using python

---
+++ fixed.py
@@ -0,0 +1,13 @@
+
+import subprocess
+
+#fixing os command execution vulnerability in python
+def command_exec():
+    allowed_commands =['ls','pwd','whoami']
+    user_input = input("Enter a command: ")
+    if user_input in allowed_commands:
+        subprocess.run (user_input)
+    else:
+        print("error. command not allowed ")
+
+command_exec()
